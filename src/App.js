f8e688017937a2694107f7a0c37acb3cd3ad4f20
Dennis.B <Baust.Dennis@gmail.com>
2024-10-05 01:40:06 +0200

    header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import './App.css';
-import Header from './header';
+import Header from './components/header';
 
 function App() {
   return (
